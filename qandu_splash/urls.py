a01c2e8d5ae6844425d08a9c3798d04b05f7d3bd
Alice <apetsiava@quinnipiac.edu>
2015-09-17 18:07:25 +0000

    Add homepage

--- qandu_splash/urls.py
+++ qandu_splash/urls.py
@@ -1,4 +1,5 @@
-"""qandu_splash URL Configuration
+"""
+qandu_splash URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
@@ -18,4 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('core.urls')),
 ]
+
